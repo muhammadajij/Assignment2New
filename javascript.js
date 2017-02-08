7c4c5a4b34837e1b2b5d2f3489ed106e1eb72b6f
Muhammad <muhammad@Muhammads-MBP.home>
2017-02-08 18:51:23 +0000

    Sixteenth commit, javascript back button configured for portfolio

--- javascript.js
+++ javascript.js
@@ -9,95 +9,90 @@ var imageNumber = 0;
 var imageLength = images.length - 1;
 
 function changeImage(x) {
-	imageNumber += x;
-	//restarts array
-	if (imageNumber > imageLength){
-		imageNumber = 0;
-	}
+  imageNumber += x;
+  //restarts array
+  if (imageNumber > imageLength) {
+    imageNumber = 0;
+  }
 
-	if (imageNumber < 0){
-		imageNumber = imageLength;
-	}
+  if (imageNumber < 0) {
+    imageNumber = imageLength;
+  }
 
-	document.getElementById("slideshow").src = images[imageNumber];
-	// document.getElementById("caption").innetHTML = caption[imageNumber];
+  document.getElementById("slideshow").src = images[imageNumber];
+  // document.getElementById("caption").innetHTML = caption[imageNumber];
 
-	return false;
+  return false;
 }
 
 function autoRun() {
-	setInterval("changeImage(1)", 5000);
+  setInterval("changeImage(1)", 5000);
 
 }
 
-        var nrImg = 3;  //the number of img , I only have 3 
-        var IntSeconds = 4;     //the seconds between the imgs
+var nrImg = 3; //the number of img , I only have 3 
+var IntSeconds = 4; //the seconds between the imgs
 
 // Portfolio
-function Load()
-        {
-            nrShown = 0;    //the img visible
-            Vect = new Array(nrImg + 10);
-            Vect[0] = document.getElementById("Img1");
-            Vect[0].style.visibility = "visible";
+function Load() {
+  nrShown = 0; //the img visible
+  Vect = new Array(nrImg + 10);
+  Vect[0] = document.getElementById("Img1");
+  Vect[0].style.visibility = "visible";
 
-            document.getElementById("S" + 0).style.visibility = "visible";
+  document.getElementById("S" + 0).style.visibility = "visible";
 
-            for (var i = 1; i < nrImg; i++)
-            {
-                Vect[i] = document.getElementById("Img" + (i + 1));
-                document.getElementById("S" + i).style.visibility = "visible";
-            }
+  for (var i = 1; i < nrImg; i++) {
+    Vect[i] = document.getElementById("Img" + (i + 1));
+    document.getElementById("S" + i).style.visibility = "visible";
+  }
 
-            document.getElementById("S" + 0).style.backgroundColor = "rgba(255, 255, 255, 0.90)";
-            document.getElementById("SP" + nrShown).style.visibility = "visible";
+  document.getElementById("S" + 0).style.backgroundColor = "rgba(255, 255, 255, 0.90)";
+  document.getElementById("SP" + nrShown).style.visibility = "visible";
 
-            mytime = setInterval(Timer, IntSeconds * 1000);
-        }
-function Timer()
-        {
-            nrShown++;
-            if (nrShown == nrImg)
-                nrShown = 0;
-            Effect();
-        }
-        //next img
-function next()
-        {
-            nrShown++;
-            if (nrShown == nrImg)
-                nrShown = 0;
-            Effect();
+  mytime = setInterval(Timer, IntSeconds * 1000);
+}
 
-            clearInterval(mytime);
-            mytime = setInterval(Timer, IntSeconds * 1000);
-        }
-function prev()
-        {
-            nrShown--;
-            if (nrShown == -1)
-                nrShown = nrImg -1;
-            Effect();
+function Timer() {
+  nrShown++;
+  if (nrShown == nrImg)
+    nrShown = 0;
+  Effect();
+}
+//next img
+function next() {
+  nrShown++;
+  if (nrShown == nrImg)
+    nrShown = 0;
+  Effect();
 
-            clearInterval(mytime);
-            mytime = setInterval(Timer, IntSeconds * 1000);
-        }
-        //here changes the img + effect
-function Effect()
-        {
-            for (var i = 0; i < nrImg; i++)
-            {
-                Vect[i].style.opacity = "0";   //to add the fade effect
-                Vect[i].style.visibility = "hidden";
+  clearInterval(mytime);
+  mytime = setInterval(Timer, IntSeconds * 1000);
+}
 
-                document.getElementById("S" + i).style.backgroundColor = "rgba(0, 0, 0, 0.70)";
-                document.getElementById("SP" + i).style.visibility = "hidden";
-            }
-            Vect[nrShown].style.opacity = "1";
-            Vect[nrShown].style.visibility = "visible";
-            document.getElementById("S" + nrShown).style.backgroundColor = "rgba(255, 255, 255, 0.90)";
-            document.getElementById("SP" + nrShown).style.visibility = "visible";
-        }
+function prev() {
+  nrShown--;
+  if (nrShown == -1)
+    nrShown = nrImg - 1;
+  Effect();
+
+  clearInterval(mytime);
+  mytime = setInterval(Timer, IntSeconds * 1000);
+}
+//here changes the img + effect
+function Effect() {
+  for (var i = 0; i < nrImg; i++) {
+    Vect[i].style.opacity = "0"; //to add the fade effect
+    Vect[i].style.visibility = "hidden";
+
+    document.getElementById("S" + i).style.backgroundColor = "rgba(0, 0, 0, 0.70)";
+    document.getElementById("SP" + i).style.visibility = "hidden";
+  }
+  Vect[nrShown].style.opacity = "1";
+  Vect[nrShown].style.visibility = "visible";
+  document.getElementById("S" + nrShown).style.backgroundColor = "rgba(255, 255, 255, 0.90)";
+  document.getElementById("SP" + nrShown).style.visibility = "visible";
+}
 
 
 // $('#link').click(function(event) {
@@ -105,9 +100,9 @@ function Effect()
 //    $.scrollTo($('.title'), 1000);
 // });
 
-   //slideshow home button
-function home(){
-    window.location.href='Home.html';
+//slideshow home button
+function home() {
+  window.location.href = 'Home.html';
 
 }
 
@@ -118,198 +113,187 @@ function home(){
 
 $(document).ready(function()
 
-            {   $(".detail").hide()
-                $(".arrowup").hide()
+  {
+    $(".detail").hide()
+    $(".arrowup").hide()
 
-            $(".arrow").click(function()
-            {
-                $(".detail").show(500);
-                $(".arrowup").show(500);
-                $(".arrow").hide();
+    $(".arrow").click(function() {
+      $(".detail").show(500);
+      $(".arrowup").show(500);
+      $(".arrow").hide();
 
-                if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+        // Store hash
+        var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function() {
 
-            })
-
-            $(".arrowup").click(function()
-            {
-                $(".detail").hide(500);
-                 $(".arrow").show(500);
-                 $(".arrowup").hide();
-
-            }
-
-            );
-
-               $(".detail2").hide()
-               $(".arrowup2").hide()
-
-            $(".arrow2").click(function()
-            {
-           if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-                $(".detail2").show(500);
-                $(".arrowup2").show(500);
-                $(".arrow2").hide();
-
-       
-            })
-
-            $(".arrowup2").click(function()
-            {
-                $(".detail2").hide(500);
-                $(".arrow2").show(500);
-                 $(".arrowup2").hide();
-            }
-
-            );
-
-               $(".detail3").hide()
-               $(".arrowup3").hide()
-
-            $(".arrow3").click(function()
-            {
-
-            	      if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-                $(".detail3").show(500);
-                $(".arrowup3").show(500);
-                $(".arrow3").hide();
-
-            })
-
-            $(".arrowup3").click(function()
-            {
-                $(".detail3").hide(500);
-                $(".arrow3").show(500);
-                 $(".arrowup3").hide();
-            }
-
-            );
-
-                $(".mainHeader").hide();
-                 $(".mainContent").hide();
-                $(".sidebars").hide();
-                 $(".title").hide();
-                 $(".searchbar").hide();
-                $("#slideshow").hide();
-                $(".lheader").show();
-
-
-
-            $(".link").click(function()
-            {
-                $(".mainHeader").show(300);
-                $(".mainContent").show(500);
-                 $(".title").show(300);
-                 $(".searchbar").show(300);
-                $("#slideshow").show(300);
-
-                $(".sidebars").show(500);
-                $(".lheader").hide(1000);
-
-
-            }
-            );
-
-            $("#land").click(function()
-            {
-
-                $(".mainHeader").hide();
-                $(".mainContent").hide();
-                 $(".title").hide();
-                 $(".searchbar").hide();
-                $("#slideshow").hide();
-
-                $(".sidebars").hide();
-                $(".lheader").show(800);
-
-
-            }
-            );
-
-
-            // $(".port").click(function()
-            // {
-
-            //     $(".mainHeader").hide(700);
-            //     $(".mainContent").hide(700);
-            //      $(".title").hide(700);
-            //      $(".searchbar").hide(700);
-            //     $("#slideshow").hide(700);
-            //     $("#slider").hide();
-
-            //     $(".sidebars").hide();
-            //     $("#slider").show(800);
-
-
-            // }
-            // );
-
-            $("#home").click(function()
-            {
-
-                $(".mainHeader").show(200);
-                $(".mainContent").show(1500);
-                 $(".title").show(900);
-                 $(".searchbar").show(100);
-                $("#slideshow").show(1100);
-
-                $(".sidebars").show(2000);
-                $(".lheader").hide();
-
-
-            }
-            );
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
         });
+      } // End if
 
-$(document).ready(function(){
+    })
+
+    $(".arrowup").click(function() {
+        $(".detail").hide(500);
+        $(".arrow").show(500);
+        $(".arrowup").hide();
+
+      }
+
+    );
+
+    $(".detail2").hide()
+    $(".arrowup2").hide()
+
+    $(".arrow2").click(function() {
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function() {
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+      $(".detail2").show(500);
+      $(".arrowup2").show(500);
+      $(".arrow2").hide();
+
+
+    })
+
+    $(".arrowup2").click(function() {
+        $(".detail2").hide(500);
+        $(".arrow2").show(500);
+        $(".arrowup2").hide();
+      }
+
+    );
+
+    $(".detail3").hide()
+    $(".arrowup3").hide()
+
+    $(".arrow3").click(function() {
+
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function() {
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+      $(".detail3").show(500);
+      $(".arrowup3").show(500);
+      $(".arrow3").hide();
+
+    })
+
+    $(".arrowup3").click(function() {
+        $(".detail3").hide(500);
+        $(".arrow3").show(500);
+        $(".arrowup3").hide();
+      }
+
+    );
+
+    $(".mainHeader").hide();
+    $(".mainContent").hide();
+    $(".sidebars").hide();
+    $(".title").hide();
+    $(".searchbar").hide();
+    $("#slideshow").hide();
+    $(".lheader").show();
+
+
+
+    $(".link").click(function() {
+      $(".mainHeader").show(300);
+      $(".mainContent").show(500);
+      $(".title").show(300);
+      $(".searchbar").show(300);
+      $("#slideshow").show(300);
+
+      $(".sidebars").show(500);
+      $(".lheader").hide(1000);
+
+
+    });
+
+    $("#land").click(function() {
+
+      $(".mainHeader").hide();
+      $(".mainContent").hide();
+      $(".title").hide();
+      $(".searchbar").hide();
+      $("#slideshow").hide();
+
+      $(".sidebars").hide();
+      $(".lheader").show(800);
+
+
+    });
+
+
+    // $(".port").click(function()
+    // {
+
+    //     $(".mainHeader").hide(700);
+    //     $(".mainContent").hide(700);
+    //      $(".title").hide(700);
+    //      $(".searchbar").hide(700);
+    //     $("#slideshow").hide(700);
+    //     $("#slider").hide();
+
+    //     $(".sidebars").hide();
+    //     $("#slider").show(800);
+
+
+    // }
+    // );
+
+    $("#home").click(function() {
+
+      $(".mainHeader").show(200);
+      $(".mainContent").show(1500);
+      $(".title").show(900);
+      $(".searchbar").show(100);
+      $("#slideshow").show(1100);
+
+      $(".sidebars").show(2000);
+      $(".lheader").hide();
+
+
+    });
+  });
+
+$(document).ready(function() {
   // Add smooth scrolling to all links
   $(".scrollbackup").on('click', function() {
 
@@ -325,8 +309,8 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
+      }, 800, function() {
+
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
@@ -337,20 +321,20 @@ $(document).ready(function(){
 });
 
 function validateForm() {
-    var w = document.forms["form"]["name"].value;
-    
-    var x = document.forms["form"]["sname"].value;
-    
+  var w = document.forms["form"]["name"].value;
 
-    var y = document.forms["form"]["email"].value;
-    
+  var x = document.forms["form"]["sname"].value;
 
-    var z = document.forms["form"]["message"].value;
-   
-    if (x == "" || w == "" || y == "" || Z == "") {
-        alert("All Fields Must Be Filled Out");
-        return false;
-    }
+
+  var y = document.forms["form"]["email"].value;
+
+
+  var z = document.forms["form"]["message"].value;
+
+  if (x == "" || w == "" || y == "" || Z == "") {
+    alert("All Fields Must Be Filled Out");
+    return false;
+  }
 
 }
 
@@ -367,8 +351,8 @@ function validateForm() {
 
 //Function To Display Popup
 function div_show() {
-//window.location.href='Home.html'
-document.getElementById('popupdiv').style.display = "block";
+  //window.location.href='Home.html'
+  document.getElementById('popupdiv').style.display = "block";
 }
 
 // function home_show() {
@@ -379,8 +363,8 @@ document.getElementById('popupdiv').style.display = "block";
 // }
 
 //Function to Hide Popup
-function div_hide(){
-document.getElementById('popupdiv').style.display = "none";
+function div_hide() {
+  document.getElementById('popupdiv').style.display = "none";
 }
 
 
@@ -424,17 +408,17 @@ document.getElementById('popupdiv').style.display = "none";
 //USE OF JSON
 function myFunction() {
 
-var nameval = document.getElementById("name").value;
-var snameval = document.getElementById("sname").value;
+  var nameval = document.getElementById("name").value;
+  var snameval = document.getElementById("sname").value;
 
-// var obj{};
-// obj['name'] = nameval;
-// obj['surname'] = surname;
+  // var obj{};
+  // obj['name'] = nameval;
+  // obj['surname'] = surname;
 
-// var x = obj.name;
-// var y = obj.surname;
+  // var x = obj.name;
+  // var y = obj.surname;
 
-document.getElementById("demo").innerHTML =  alert("Thank You " + nameval + " " + snameval + " For Your Message Please Close The Contact Form");
+  document.getElementById("demo").innerHTML = alert("Thank You " + nameval + " " + snameval + " For Your Message Please Close The Contact Form");
 
 }
 
@@ -459,20 +443,22 @@ document.getElementById("demo").innerHTML =  alert("Thank You " + nameval + " " 
 
 //timestamp
 function time() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('livetimes').innerHTML =
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('livetimes').innerHTML =
     h + ":" + m + ":" + s;
-    var t = setTimeout(time, 500);
+  var t = setTimeout(time, 500);
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {
+    i = "0" + i
+  }; // add zero in front of numbers < 10
+  return i;
 }
 
 
@@ -487,10 +473,21 @@ function checkTime(i) {
 //   }
 // }
 
-function swapStyleSheet(sheet){
-   
-    document.getElementById("dark").setAttribute("href", sheet);
+function swapStyleSheet(sheet) {
 
-   }
+  document.getElementById("dark").setAttribute("href", sheet);
 
+}
+
+function goBack() {
+    window.history.back();
+}
+
+//fixes nav bar using jquery
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() > 80) {
+//         $('.mainHeader').css('top', $(window).scrollTop());
+//     }
+// }
+// );
 
